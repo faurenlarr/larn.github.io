e84098d06826010adb9e58d6585cec4defeee468
Lauren Farr <laurenfarr@Lauren-MacBook-Pro.local>
2015-12-21 15:41:20 -0500

    more pages

---
+++ main.js
@@ -0,0 +1,67 @@
+
+
+$(window).scroll(function() {
+
+
+    if ($(this).scrollTop() <= $('#sectiona').offset().top - 50) {
+        // $( ".navbar" ).fadeOut(500);
+    }
+
+    ///Block A
+    if ($(this).scrollTop() >= $('#sectiona').offset().top - 25) {
+        // $( ".navbar" ).fadeIn(500);
+        $( ".pic" ).removeClass('hidden').addClass('show slide');
+        $(".words").removeClass('hidden').addClass('show slide');
+      //  $('nav a').stop().removeClass('active');
+      //  $('nav a:eq(0)').stop().addClass('active');
+    }
+
+    ///Block B
+  //   if ($(this).scrollTop() >= $('#sectionb').offset().top - 25) {
+  //     $('nav a').stop().removeClass('active');
+  //     $('nav a:eq(1)').stop().addClass('active');
+  //     $( ".projectA, .projectB, .projectC, .projectD" ).removeClass('invis').addClass('vis popped');
+  //     $('#barB').stop().animate({ left: '0px' });
+  // } else {
+  //     $('#barB').stop().animate({ left: '-1000px' });
+  // }
+
+
+    ///Block C
+  //   if ($(this).scrollTop() >= $('#blockC').offset().top - 25) {
+  //     $('nav a').removeClass('active');
+  //     $('nav a:eq(2)').addClass('active');
+  //     $('#barC').stop().animate({ left: '0px' });
+  //     $("#bye, #hold").show();
+  // } else {
+  //       $('#barC').stop().animate({ left: '-1000px' });
+  //       $('#bye, #hold').hide();
+  //   }
+
+    ///Block D
+  //   if ($(this).scrollTop() >= $('#blockD').offset().top - 25) {
+  //     $('nav a').removeClass('active');
+  //     $('nav a:eq(3)').addClass('active');
+  // } else {
+  //
+  // }
+});
+
+
+
+(function() {
+
+
+      $(".header").on("mouseover", ".fa-arrow-down", function() {
+          $('html, body').animate({
+              scrollTop: $("#sectiona").offset().top }, 1000);
+          });
+
+
+
+
+
+
+
+
+}());
