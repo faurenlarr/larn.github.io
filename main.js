ff114a5d005c3f846bac160067c24afa63cf5bd0
Lauren Farr <lmfarr123@gmail.com>
2017-04-03 20:39:24 -0400

    remove commented out code

--- main.js
+++ main.js
@@ -2,32 +2,12 @@
 
 $(window).scroll(function() {
 
-
-
     if ($(this).scrollTop() >= $('.sectiona').offset().top - 25) {
         $( ".navbar" ).fadeIn(500);
         $(".pic").removeClass('hidden').addClass('show slide');
         $('.info').removeClass('hidden').addClass('show slide');
         $('.tabs').removeClass('hidden').addClass('show slide');
-        // $('.projects').removeClass('hidden').addClass('show slide');
-        // $('.resume').removeClass('hidden').addClass('show slide');
-        // $('.contab').removeClass('hidden').addClass('show slide');
     }
-
-
-
-    if ($(this).scrollTop() >= $('.sectionb').offset().top - 25) {
-      // $('.one').removeClass('hidden').addClass('show slide');
-      // $('.two').removeClass('hidden').addClass('show slide');
-      // $('.three').removeClass('hidden').addClass('show slide');
-      // $('.viewother').removeClass('hidden').addClass('show slide');
-      // $('.circles').removeClass('hidden').addClass('slide');
-
-    }
-
-    // if ($(this).scrollTop() >= $('.sectionc').offset().top-25) {
-    //
-    // }
 
 });
 
